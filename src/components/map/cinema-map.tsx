653754b4f8af8d03ef0a6507ebc33811050daebf
theo jorge <theojorge2001@gmail.com>
2026-08-26 13:00:36 -0300

    change map style

--- src/components/map/cinema-map.tsx
+++ src/components/map/cinema-map.tsx
@@ -71,8 +71,8 @@ export function CinemaMap({ cines, visibleSlugs, selectedSlug, loaded, onSelect 
       scrollWheelZoom
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <ZoomControl position="bottomright" />
       <InvalidateSize />
